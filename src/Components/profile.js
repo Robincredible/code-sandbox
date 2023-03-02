27ea28841c29ae12b7fe617dcf230d48afcd13bb
Robin Del Mundo <batbord94@gmail.com>
2023-03-02 07:54:33 +0000

    Update 9 files

--- src/Components/profile.js
+++ src/Components/profile.js
@@ -4,13 +4,11 @@ import defaultImage from "../Images/image-jeremy.png";
 
 const Profile = (props) => {
   return (
-    <div>
-      <Card className="profile">
-        <img src={props.image || defaultImage} />
-        <p>Report for</p>
-        <h1>{props.name || "Jeremy Robson"}</h1>
-      </Card>
-    </div>
+    <Card className="profile">
+      <img src={props.image || defaultImage} />
+      <p>Report for</p>
+      <h1 className="name">{props.name || "Jeremy Robson"}</h1>
+    </Card>
   );
 };
 
--- src/Components/stats.js
+++ src/Components/stats.js
@@ -1,14 +1,27 @@
 import Card from "./UI/card";
+import ellipsis from "../Images/icon-ellipsis.svg";
+
 import "./stats.css";
 
 const Stats = (props) => {
   const timeframeSelected = props.selected.toLowerCase();
 
+  const sanitizedTitle = (string) => {
+    return string.replace(/\W+/g, "-").toLowerCase();
+  };
+
   return (
-    <Card>
-      <h1>{props.title}</h1>
-      <h2>{props.timeframes[timeframeSelected].current}</h2>
-      <h3>Last Week - {props.timeframes[timeframeSelected].previous} Hours</h3>
+    <Card className={"stats " + sanitizedTitle(props.title.toLowerCase())}>
+      <div class="flex-title">
+        <h1 className="stat-title">{props.title}</h1>
+        <img className="ellipsis" src={ellipsis} />
+      </div>
+      <h2 className="stat-hours">
+        {props.timeframes[timeframeSelected].current}hrs
+      </h2>
+      <h3 className="stat-hours-prev">
+        Last Week - {props.timeframes[timeframeSelected].previous} Hours
+      </h3>
     </Card>
   );
 };
